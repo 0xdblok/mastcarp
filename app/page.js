97589937ff02c8eb5c54cf05c2081e863c56bed4
dblok0x <dblok0x@gmail.com>
2024-11-25 22:21:52 +0100

    ee

--- app/page.js
+++ app/page.js
@@ -3,14 +3,14 @@ import log from "./log.jpg";
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center   px-20  pb-36 md:pb-10 text-xl text-black text-center white max-md:px-5 max-md:pt-4">
+    <section className="flex flex-col items-center    px-20  pb-36 md:pb-10 text-xl text-black text-center white max-md:px-5 max-md:pt-4">
     <div className="md:flex md:flex-col  md:w-full  ">
      <div className="flex flex-col justify-center items-center">
-     <Image className="ml-4 pt-8"  
+     <Image className=" pt-8"  
      src={log} alt="Skinny" 
      width={140} 
      height={140}/>
-     <p className="text-white ">Mastcarp</p>
+     <p className="text-zinc-900 text-xl  ">Mastcarp</p>
    
    
       </div>
@@ -30,7 +30,7 @@ export default function Home() {
          <article className="flex flex-col items-center   text-zinc-900 pt-64">
 
 
-       <nav className="flex gap-3.5 items-center mt-80 max-w-full text-base w-[248px] max-md:mt-10">
+       <nav className="flex gap-3.5 items-center mt-30 max-w-full text-base w-[248px] max-md:mt-10">
          <a href="" className="grow self-stretch my-auto hover:underline focus:outline-none focus:ring-2 focus:ring-offset-2">
            twitter
          </a>
@@ -47,3 +47,4 @@ export default function Home() {
    </section>
   );
 }
+
